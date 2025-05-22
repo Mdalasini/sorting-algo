fc7fd284d99f8d6a5a1e4719865b46cf8bad556a
Mdalasini <pathsofgrassymonkey@gmail.com>
2025-05-22 23:39:24 +0300

    feat(config): Move colors to config.js for accessibility

---
+++ js/config.js
@@ -0,0 +1,9 @@
+export const colors = {
+  lightGreen: "oklch(76.5% 0.177 163.223)",
+  green: "oklch(62.7% 0.194 149.214)",
+  cyan: "oklch(68.5% 0.169 237.323)",
+  yellow: "oklch(90.5% 0.182 98.111)",
+  orange: "oklch(76.9% 0.188 70.08)",
+  pink: "oklch(80.8% 0.114 19.571)",
+  red: "oklch(63.7% 0.237 25.331)",
+};
