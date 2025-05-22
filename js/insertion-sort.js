25ce8b083d53041894530467bcb89e5c6e29d7f6
Mdalasini <pathsofgrassymonkey@gmail.com>
2025-05-23 00:54:12 +0300

    feat(insertion-sort): Add styling and animations

--- js/insertion-sort.js
+++ js/insertion-sort.js
@@ -1,4 +1,5 @@
-import { copyBarProperties } from "./helpers.js";
+import { colors } from "./config.js";
+import { copyBarProperties, sleep } from "./helpers.js";
 
 /**
  * Sorts an array of bar elements visually using the insertion sort algorithm
@@ -8,13 +9,29 @@ import { copyBarProperties } from "./helpers.js";
 async function insertionSort(bars) {
   for (let i = 1; i < bars.length; i++) {
     let key = bars[i].cloneNode(true);
+    bars[i].style.background = colors.orange; // current key
     let j = i - 1;
 
     while (j >= 0 && parseInt(bars[j].dataset.value) > parseInt(key.dataset.value)) {
+      bars[j].style.background = colors.yellow; // comparing
+      bars[j + 1].style.background = colors.yellow; // shift
+      await sleep(100);
+
       copyBarProperties(bars[j + 1], bars[j]);
+      bars[j].style.background = colors.cyan; // reset 
+      bars[j + 1].style.background = colors.cyan;
       j--;
     }
+
     copyBarProperties(bars[j + 1], key);
+    bars[j + 1].style.background = colors.lightGreen; // placed
+    await sleep(100);
+  }
+
+  // Final pass to mark all as sorted
+  for (let bar of bars) {
+    bar.style.background = colors.green;
+    await sleep(30);
   }
 }
 
