8b031cd5bc539c954b10721944cb3090148457c4
Mdalasini <pathsofgrassymonkey@gmail.com>
2025-05-23 00:46:36 +0300

    feat(quick-sort): Add styling and animations

--- js/quick-sort.js
+++ js/quick-sort.js
@@ -1,4 +1,5 @@
 import { sleep, swapBars } from "./helpers.js";
+import { colors } from "./config.js";
 
 /**
  * Partitions the array around a pivot element for quick sort
@@ -9,18 +10,24 @@ import { sleep, swapBars } from "./helpers.js";
  */
 async function partition(bars, low, high) {
   let pivot = bars[high];
+  bars[high].style.background = colors.red; // Highlight pivot
   let i = low - 1;
 
   for (let j = low; j <= high - 1; j++) {
+    bars[j].style.background = colors.yellow; // Highlight comparision
+    await sleep(200);
 
     if (parseInt(bars[j].dataset.value) < parseInt(pivot.dataset.value)) {
       i++;
       await swapBars(bars[i], bars[j]);
     } 
+
+    bars[j].style.background = colors.cyan; // reset color after comparision
   }
 
   await swapBars(bars[i + 1], bars[high]);
 
+  bars[high].style.background = colors.cyan; // reset pivot color
   return i + 1;
 }
 
@@ -34,9 +41,13 @@ async function partition(bars, low, high) {
 async function _quickSort(bars, low, high) {
   if (low < high) {
     let pi = await partition(bars, low, high);
+    bars[pi].style.background = colors.green; // Mark pivot as sorted
 
     await _quickSort(bars, low, pi - 1);
     await _quickSort(bars, pi + 1, high);
+  } else if (low >= 0 && high >= 0 && low < bars.length && high < bars.length) {
+    bars[low].style.background = colors.green; // Single element is sorted
+    bars[high].style.background = colors.green;
   }
 }
 
